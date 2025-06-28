87d300e4c8fb922ea24d80e9cfd1beaf199b426e
poursa <porsa0@hotmail.com>
2025-06-28 23:29:44 +0300

    remove

--- src/learning_greek_bot/data/faq_answers.py
+++ src/learning_greek_bot/data/faq_answers.py
@@ -1,9 +1,6 @@
 faq_answers = {
 'reference grammar':
 """
-Reference grammars: https://drive.google.com/file/d/0B8gQTdaVKV05dnFDcF92QUotNFE/view?resourcekey=0-fJA82OndcZO0Z097JbMLWQ
-
-                    https://drive.google.com/file/d/0B8gQTdaVKV05MGlaTWsxU3hIa2M/view?resourcekey=0-niA34muYLDGq8yZXN_C0Xw
 """,
 'test1':
 """
